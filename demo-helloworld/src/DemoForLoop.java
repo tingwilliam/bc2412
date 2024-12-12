37d76d71e9e2c16eb7f7c629bb0c4346194143c4
William.ting <tinghonam@gmail.com>
2024-12-12 16:42:43 +0800

    20241212

--- demo-helloworld/src/DemoForLoop.java
+++ demo-helloworld/src/DemoForLoop.java
@@ -41,34 +41,85 @@ public class DemoForLoop {
       }
     }
     // print 0 - 100, divided by 3 and divided by 4
-    for (int i = 0; i < 101; i++) 
-      if (i % 3 == 0 && i % 4 == 0) 
+    for (int i = 0; i < 101; i++) {
+      if (i % 3 == 0 && i % 4 == 0)
         System.out.println(i);
-      
+    }
+
     // sum up 0 - 20: 0 + 1 + 2... + 20
     int sum = 0;
-    for (int i = 0; i < 21; i++) //i=i+1 
+    for (int i = 0; i < 21; i++) // i=i+1
     {
-      sum += i;  // sum = sum + i;
+      sum += i; // sum = sum + i;
     }
     System.out.println(sum); // 210
     // System.out.println(i); ! i is declared within the for loop
-  
-    //sum up all odd number between 0 - 10
-    sum = 0;
-    for (int i = 0; i <11; i++) {
-    if (i % 2== 1) 
-      sum += i;
+
+    // sum up all odd number between 0 - 10
+    // sum up all even number between 0 - 10
+    // ! Find the difference between evenSum and oddSum -> positive number
+    int oddSum = 0;
+    int evenSum = 0;
+    for (int i = 0; i < 11; i++) {
+      if (i % 2 == 1) {
+        oddSum += i;
+    }else {
+    evenSum += i;
+      }
     }
-      System.out.println(sum);
-    
-  
+    //int diff = -1;
+    // if (oddSum > evenSum) {
+    //   diff = oddSum - evenSum;
+    //} else {
+    //   diff = evenSum - oddSum;
+    //}
+    int diff = oddSum > evenSum ? oddSum -evenSum : evenSum - oddSum;
+    System.out.println(diff); // 5
+
+    for (int i2 = 0; i2 < 11; i2++) {
+      if (i2 % 2 == 0)
+        System.out.println(sum);
+    }
+
+
+    // Searching
+    String str = "abcedfghi";
+
+    // 1. Check if 'd' exists in the string.
+    // for loop + if +chart
+    boolean found = false;
+    for (int i = 0; i < str.length(); i++) {
+      if (str.charAt(i) == 'd') {
+        found = true;
+        break; // break the nearest loop
+      }
+    }
+
+    System.out.println(found);
+
+    // Test Case:
+    // 1. "abcdefghi"
+    // 2. "abcefghi"
+    // 3. " "
+    // 4. "abcdefghijkd"
+
+    // 2. check if the string value contains given sub-string
+    String substr = "ll";
+    String str2 = "hello";
+
+    boolean isSubstringExist = false;
+    for (int i = 0; i < str2.length() - substr.length(); i++) {
+      if (str2.substring(i, i + substr.length()).equals(substr)) {
+        isSubstringExist = true;
+        break;
+      }
+    }
+    // for loop + substring
+    System.out.println(isSubstringExist); // true
 
 
 
-
-
-
+  }
 }
 
-}
+
